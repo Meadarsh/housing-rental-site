c609292240b1d45118a68d85cf03eaa944b31c6b
meadarsh <itsadarsh33@gmail.com>
2024-03-08 21:48:21 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import NavBar from "./Components/navBar";
 import Home from "./Home/home";
-import { BrowserRouter, Routes,Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes,Route, useLocation } from "react-router-dom";
 import Rent from "./Rent/Rent";
 import PG from "./Pg/Pg";
 import AdminPage from "./Admin/AdminPage";
 import Contact from "./contact/contact";
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
+  
 
   return (
     <>
     
     <BrowserRouter>
+    <ScrollToTop />
     <NavBar />
     <Routes>
       <Route path="/" element={<Home/>} />
