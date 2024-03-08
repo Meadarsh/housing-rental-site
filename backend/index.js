d834269fe65f277619dc4498baa9d0972a29e9fc
meadarsh <itsadarsh33@gmail.com>
2024-03-08 18:06:47 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT||3001;
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:process.env.ORIGIN
 }))
  // Connect to the database
 
