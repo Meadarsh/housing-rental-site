a1d5c7e9c6728df70c23a347531d963e3943d0de
meadarsh <itsadarsh33@gmail.com>
2024-03-08 16:59:00 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,32 @@ import  express  from "express";
 import cors from "cors"
 import mongoose from "mongoose";
 import {v2  as cloudinary} from 'cloudinary'
-import { Flats } from "./schema/Schema.js";
+import { Schema, model } from "mongoose";
+
+const FlatSchema= new Schema({
+        rentalType: String, 
+        rent: String,
+        singleSharingRent:String,
+        twinSharingRent:String,
+        securityDeposit: String,
+        address: String,
+        city: String,
+        state: String,
+        carpetArea:String,
+        furnishing:String,
+        flatType:String,
+        bathroom:String,
+        parking: String,
+        balcony:String,
+        totalFloor: String,
+        onFloor: String,
+        tenantsPreferred:String,
+        selectedItems: Array,
+        selectedBuildingItems:Array,
+        amenities: Object,
+        imageUrl:Array
+})
+const Flats = model("Flats", FlatSchema); 
 
 cloudinary.config({
   cloud_name: 'cloud-space',
