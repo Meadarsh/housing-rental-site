9a02a4f78e9b64a7f73b6dcd6013a64883e42e01
meadarsh <itsadarsh33@gmail.com>
2024-03-10 01:50:23 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,38 @@ import dotenv from 'dotenv'
 import  express  from "express";
 import cors from "cors"
 import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
 import {v2  as cloudinary} from 'cloudinary'
-import { Flats } from "./schema/Schema.js";
 dotenv.config({
   path:'./.env'
 })
+const FlatSchema = new Schema({
+  rentalType: String,
+  rent: String,
+  singleSharingRent: String,
+  twinSharingRent: String,
+  threeSharingRent: String,
+  fourSharingRent: String,
+  securityDeposit: String,
+  address: String,
+  city: String,
+  state: String,
+  carpetArea: String,
+  furnishing: String,
+  flatType: String,
+  bathroom: String,
+  parking: String,
+  balcony: String,
+  totalFloor: String,
+  onFloor: String,
+  tenantsPreferred: String,
+  selectedItems: Array,
+  selectedBuildingItems: Array,
+  amenities: Object,
+  imageUrl: Array,
+});
+const Flats = model("Flats", FlatSchema);
+
 
 cloudinary.config({
   cloud_name: 'cloud-space',
