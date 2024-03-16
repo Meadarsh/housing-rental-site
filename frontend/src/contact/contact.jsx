3682ac0e2377a1e2434fe1535f70d439232f8d4b
meadarsh <itsadarsh33@gmail.com>
2024-03-16 19:05:02 +0530

    new

--- frontend/src/contact/contact.jsx
+++ frontend/src/contact/contact.jsx
@@ -11,7 +11,6 @@ const Contact = () => {
      <MessageBox/>
         <div className='flex flex-col pl-3 items-center border-l-2'>
         <h1 className='text-red-800 text-[8vw] lg:text-[3vw] mt-20'>Contact Us</h1>
-        <h1 className='text-red-800 lg:text-2xl text-xl'>Have questions or need assistance?</h1>
         <div className="border border-black rounded-lg mb-[15vh] lg:mb-[2vh] h-[30vh] flex items-center justify-center gap-8 flex-col mt-10 w-[90%] lg:w-[30vw]">
         <div className='flex justify-center items-center gap-3 text-3xl'><FaPhoneAlt className='text-3xl text-red-800'/> <p>+918652869869</p></div>
         <a href="https://wa.me/+918652869869"><div className='flex justify-center items-center gap-3 text-3xl'><FaWhatsapp className='text-4xl text-green-600'/><p>Whatsapp</p> </div></a>
